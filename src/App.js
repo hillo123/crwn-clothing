eee2673b793c1cac078044fbe6ea70845401913c
Guillermo <guillermozayas2002@gmail.com>
2022-09-20 23:29:45 -0400

    separating directory and items from home page. adding style and bgd imgs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './homepage.styles.scss';
-import HomePage from './homepage.component';
+import HomePage from  './pages/homepage/homepages.component';
 
 function App() {
   return (
